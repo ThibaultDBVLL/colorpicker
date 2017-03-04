648a9580db43712ad48f5666b63e7680724ebfa3
thibault <thibault.daboville@gmail.com>
2017-03-04 21:51:48 +0100

    valeur initiale aux inputs dans le js

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@ $( document ).ready(function() {
     var r = 125;
     var g = 125;
     var b = 125;
+    var r2 = 125;
+    var g2 = 125;
+    var b2 = 125;
+    $('#body').css({'background-color': 'rgb('+ r + ', ' +  g + ', ' + b +')'});
 
     $('#r1').on("change mousemouve",function(){
         r = $(this).val();
